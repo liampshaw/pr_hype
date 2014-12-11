c44b198d6a83129149ed3e5828d22bbaeca5db7f
Liam Shaw <liam@pc241-189.ich.ucl.ac.uk>
2014-12-11 15:45:40 +0000

    Now searches and returns authors from PubMed

---
+++ getpubmed.m
@@ -0,0 +1,59 @@
+function pmstruct = getpubmed(searchterm,varargin)
+% GETPUBMED Search PubMed database & write results to MATLAB structure
+% Error checking for required input SEARCHTERM
+if(nargin<1)
+    error('GETPUBMED:NotEnoughInputArguments',...
+          'SEARCHTERM is missing.');
+end
+% Set default settings for property name/value pairs, 
+% 'NUMBEROFRECORDS' and 'DATEOFPUBLICATION'
+maxnum = 50; % NUMBEROFRECORDS default is 50
+pubdate = ''; % DATEOFPUBLICATION default is an empty string
+% Parsing the property name/value pairs 
+num_argin = numel(varargin);
+for n = 1:2:num_argin
+    arg = varargin{n};
+    switch lower(arg)
+        
+        % If NUMBEROFRECORDS is passed, set MAXNUM
+        case 'numberofrecords'
+            maxnum = varargin{n+1};
+        
+        % If DATEOFPUBLICATION is passed, set PUBDATE
+        case 'dateofpublication'
+            pubdate = varargin{n+1};          
+            
+    end     
+end
+% Create base URL for PubMed db site
+baseSearchURL = 'http://www.ncbi.nlm.nih.gov/sites/entrez?cmd=search';
+% Set db parameter to pubmed
+dbOpt = '&db=pubmed';
+
+% Set term parameter to SEARCHTERM and PUBDATE 
+% (Default PUBDATE is '')
+termOpt = ['&term=',searchterm,'+AND+',pubdate];
+
+% Set report parameter to medline
+reportOpt = '&report=medline';
+
+% Set format parameter to text
+formatOpt = '&format=text';
+
+% Set dispmax to MAXNUM 
+% (Default MAXNUM is 50)
+maxOpt = ['&dispmax=',num2str(maxnum)];
+% Create search URL
+searchURL = [baseSearchURL,dbOpt,termOpt,reportOpt,formatOpt,maxOpt];
+medlineText = urlread(searchURL);
+hits = regexp(medlineText,'PMID-.*?(?=PMID|</pre>$)','match');
+pmstruct = struct('PubMedID','','PublicationDate','','Title','',...
+             'Abstract','','Authors','','Citation','');
+for n = 1:numel(hits)
+    pmstruct(n).PubMedID = regexp(hits{n},'(?<=PMID- ).*?(?=\n)','match', 'once');
+    pmstruct(n).PublicationDate = regexp(hits{n},'(?<=DP  - ).*?(?=\n)','match', 'once');
+    pmstruct(n).Title = regexp(hits{n},'(?<=TI  - ).*?(?=PG  -|AB  -)','match', 'once');
+    pmstruct(n).Abstract = regexp(hits{n},'(?<=AB  - ).*?(?=AD  -)','match', 'once');
+    pmstruct(n).Authors = regexp(hits{n},'(?<=AU  - ).*?(?=\n)','match');
+    pmstruct(n).Citation = regexp(hits{n},'(?<=SO  - ).*?(?=\n)','match', 'once');
+end
--- worstOffendersSample.m
+++ worstOffendersSample.m
@@ -1,9 +1,10 @@
 load('UniPR.mat');
 fileID = fopen('worstOffendersSample.txt', 'w');
-fprintf(fileID, 'Reference\tJournalTitle\tPRTitle\tSample_journal\tSample_PR\n');
+fprintf(fileID, 'Reference\tJournalTitle\tAuthors\tPRTitle\tSample_journal\tSample_PR\tPR_exaggerationScore\n');
 
 % Loop through struct and check if sample changes between journal and PR
 for n=1:462,
+    disp(n)
     % PR sample different from abstract of journal is coded as 2
     if PRs(n).PR.Sample.SameAsAbstract == 2
         % create strings for writing to file
@@ -12,10 +13,27 @@ for n=1:462,
 	    PRTitle = strrep(PRs(n).Info.PRTitle,sprintf('\n'), ' ');
 	    sample_Journal = strrep(PRs(n).Journal.Sample.Sample,sprintf('\n'), ' ');
 	    sample_PR = strrep(PRs(n).PR.Sample.Sample,sprintf('\n'), ' ');
+        
+        % get details of authors from PubMed
+        search_term =strrep(journalTitle,sprintf(' '), '+');
+        pubmed_data = vertcat(getpubmed(search_term));
+        if size(pubmed_data) == [1 1]
+            if isempty(pubmed_data.Authors) == 0
+                authors = strjoin(pubmed_data.Authors, ',');
+            else
+                authors = 'PubMed data not found'
+            end
+        else
+            authors = 'Too many PubMed results to identify'
+        end
+        
+       
         % write strings to file
-	    fprintf(fileID, '%s\t%s\t%s\t%d: %s\t%d: %s\n', reference, journalTitle, ...
-            PRTitle, PRs(n).Journal.Sample.Code, sample_Journal, ...
+	    fprintf(fileID, '%s\t%s\t%s\t%s\t%d: %s\t%d: %s\n', reference, journalTitle, ...
+            authors,...        
+    PRTitle, PRs(n).Journal.Sample.Code, sample_Journal, ...
             PRs(n).PR.Sample.Code, sample_PR);
+        
     end 
         
 end
